c96eda2c293cae8e77bc2b5f3d2668dc5a737563
Paul Maynard <Paul.Maynard001@umb.edu>
2019-12-11 11:56:04 -0500

    assembler

---
+++ assemble.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#
+# $Id: hmmmAssembler.py,v 1.4 2007/10/08 08:10:11 geoff Exp $
+#
+# hmmmAssembler.py
+# Ran Libeskind-Hadas, 2006
+# modified by Peter Mawhorter, June 2006
+# Extensively modified by Geoff Kuenning, October 2007
+# modified by Kaya Woodall, June 2012
+# rewritten by Paul Maynard, December 2019
+
+import re
+from array import array
+from hmmm import *
+import sys
+
+def parse(prog):
+    for line in prog.split('\n'):
+        line = line.partition('#')[0].strip()
+        if line:
+            line, instr, args = re.fullmatch(r'(\d+)\s+(\w+)(?:\s+([\w -]+)|)', line).groups()
+            line = int(line)
+            if args:
+                args = re.split(r'\s+', args)
+            else:
+                args = []
+            yield line, instr, args
+
+def mkinstr(name, args):
+    name = aliases.get(name, name)
+    op = opdict[name]
+    fields = arguments[name]
+    i = 0
+    offs = 12
+    for f in fields:
+        if f == 'z':
+            offs -= 4
+            continue
+        elif f == 'r':
+            offs -= 4
+            arg = args[i]
+            if arg[0] != 'r': raise ValueError
+            arg = int(arg[1:])
+            if not (0 <= arg <= 15): raise ValueError
+            op |= (arg & 0xF) << offs
+        elif f == 'u':
+            offs -= 8
+            arg = int(args[i], base=0)
+            if not (0 <= arg <= 255): raise ValueError
+            op |= (arg & 0xFF) << offs
+        elif f == 's':
+            offs -= 8
+            arg = int(args[i], base=0)
+            if not (-128 <= arg <= 127): raise ValueError
+            op |= (arg & 0xFF) << offs
+        elif f == 'n':
+            arg = int(args[i], base=0)
+            op = arg & 0xFFFF
+        i += 1
+    return op
+
+def assemble(lines):
+    out = array('H')
+    for line, name, args in lines:
+        if len(out) <= line:
+            out.extend([0] + [0] * (line - len(out)))
+        out[line] = mkinstr(name, args)
+    # if sys.byteorder == 'little': out.byteswap()
+    return out.tobytes()
+
+if __name__ == "__main__":
+    print(assemble(parse(f"""
+    0 {sys.argv[1]}
+    """)).hex())
---
+++ hmmm.py
@@ -0,0 +1,78 @@
+from itertools import zip_longest
+
+opcodes = [
+    (0x0000, 0xFFFF, "halt"),
+    (0x0001, 0xF0FF, "read"),
+    (0x0002, 0xF0FF, "write"),
+    (0x0003, 0xF0FF, "jumpi"),
+    (0x1000, 0xF000, "loadn"),
+    (0x2000, 0xF000, "load"),
+    (0x2000, 0xF000, "store"),
+    (0x4000, 0xF00F, "loadi"),
+    (0x4001, 0xF00F, "storei"),
+    (0x5000, 0xF000, "addn"),
+    (0x6000, 0xFFFF, "nop"),
+    (0x6000, 0xF00F, "mov"),
+    (0x6000, 0xF000, "add"),
+    (0x7000, 0xF0F0, "neg"),
+    (0x7000, 0xF000, "sub"),
+    (0x8000, 0xF000, "mul"),
+    (0x9000, 0xF000, "div"),
+    (0xA000, 0xF000, "mod"),
+    (0xB000, 0xFF00, "jump"),
+    (0xB000, 0xF000, "call"),
+    (0xC000, 0xF000, "jeqz"),
+    (0xD000, 0xF000, "jnez"),
+    (0xE000, 0xF000, "jgtz"),
+    (0xF000, 0xF000, "jltz"),
+    (0x0000, 0x0000, "data"),
+]
+
+opdict = {name: code for code, _, name in opcodes}
+
+aliases = {
+    "setn":"loadn",
+    "copy":"mov",
+    "jumpn":"jump",
+    "jeqzn":"jeqz",
+    "jnezn":"jnez",
+    "jgtzn":"jgtz",
+    "jltzn":"jltz",
+    "calln":"call",
+    "jump":"jumpi",
+    "jumpr":"jumpi",
+    "loadn":"load",
+    "storen":"store",
+    "load":"loadi",
+    "loadr":"loadi",
+    "store":"storei",
+    "storer":"storei"
+}
+
+arguments = {
+    "halt": "",
+    "read": "r",
+    "write": "r",
+    "jumpi": "r",
+    "loadn": "rs",
+    "load": "ru",
+    "store": "ru",
+    "loadi": "rr",
+    "storei": "rr",
+    "addn": "rs",
+    "add": "rrr",
+    "mov": "rr",
+    "nop": "",
+    "sub": "rrr",
+    "neg": "rzr",
+    "mul": "rrr",
+    "div": "rrr",
+    "mod": "rrr",
+    "jump": "zu",
+    "call": "ru",
+    "jeqz": "ru",
+    "jgtz": "ru",
+    "jltz": "ru",
+    "jnez": "ru",
+    "data": "n"
+}
